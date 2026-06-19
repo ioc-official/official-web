31b7b4b9707a837105ea0878e475231d92647d54
Indonesia Overfreight Community <alifiandranaufal2606++@gmail.com>
2026-06-19 16:02:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,14 +29,14 @@
     ],
     team: [
       { name: 'Gerald Jonathan William', role: 'Owner', avatar: './assets/LogoIOC.png' },
-      { name: 'Irgan Arda Turan', role: 'Wakil', avatar: '👑' },
-      { name: '-', role: 'Security 1', avatar: '⚒️' },
-      { name: '-', role: 'Security 2', avatar: '⚒️' },
-      { name: '-', role: 'Admin 1', avatar: '🛡️' },
-      { name: '-', role: 'Admin 2', avatar: '🛡️' },
-      { name: '-', role: 'Admin 3', avatar: '🛡️' },
-      { name: '-', role: 'Admin 4', avatar: '🛡️' },
-      { name: '-', role: 'Admin 5', avatar: '🛡️' }
+      { name: 'Irgan Arda Turan', role: 'Wakil', avatar: '' },
+      { name: '-', role: 'Security 1', avatar: '' },
+      { name: '-', role: 'Security 2', avatar: '' },
+      { name: '-', role: 'Admin 1', avatar: '' },
+      { name: '-', role: 'Admin 2', avatar: '' },
+      { name: '-', role: 'Admin 3', avatar: '' },
+      { name: '-', role: 'Admin 4', avatar: '' },
+      { name: '-', role: 'Admin 5', avatar: '' }
     ],
     stats: {
       member: 0,
